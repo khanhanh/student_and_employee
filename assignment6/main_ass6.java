3f9224463fa4b3095e624c1ebc28949568aa5136
khanhanh <44283159+khanhanh@users.noreply.github.com>
2019-05-05 23:44:25 +0700

    Add files via upload

---
+++ assignment6/main_ass6.java
@@ -0,0 +1,14 @@
+package assignment6;
+import javafx.application.Application;
+import javafx.stage.Stage;
+public class main_ass6 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
